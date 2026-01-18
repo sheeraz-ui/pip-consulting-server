43f3b08dc31dd9e063f5bd3a596fa0cd93fbff64
Sheeraz <sheerazulla03@gmail.com>
2026-01-18 13:56:36 +0530

    secl commit

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
+app.set('trust proxy', 1)
+
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 app.use(cookieParser())
